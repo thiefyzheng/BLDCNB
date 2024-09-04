76b0010d4a968a26508c3552c2bce842aea7459e
thiefyzheng <thiefyzheng@gmail.com>
2024-09-04 13:48:03 +0800

    Removed Serial bits

--- Encoder_Display_Test/Encoder_Display_Test.ino
+++ Encoder_Display_Test/Encoder_Display_Test.ino
@@ -2,57 +2,55 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-
-int counter=0;
-String dir="";
-unsigned long last_run=0;
+int counter = 0;
+int current = counter;
+String dir = "";
+unsigned long last_run = 0;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(3), shaft_moved, FALLING);
   pinMode(4, INPUT);
-  Serial.begin(115200);
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))  {
-    Serial.println("SSD1306 allocation failed");
-    for(;;);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    for (;;);
   }
+  display.setTextSize(5);
+  display.setCursor(32, 16);
   display.clearDisplay();
   display.setTextColor(WHITE);
-
+  display.println(counter);
+  display.display();
 }
 
-void shaft_moved()  {
-  if (millis()-last_run>5)  {
-    if (digitalRead(4)==1)  {
-      if (counter<25) { 
+void shaft_moved() {
+  if (millis() - last_run > 5) {
+    if (digitalRead(4) == 1) {
+      if (counter < 25) {
         counter++;
-        dir="CW";
+        dir = "CW";
       }
-    }
-    
-    if (digitalRead(4)==0)  {
-      if (counter>-25)  {
+    } else {
+      if (counter > -25) {
         counter--;
-        dir="CCW";
+        dir = "CCW";
       }
     }
-    last_run= millis();
+    last_run = millis();
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  display.setCursor(64,32);
-  display.clearDisplay();
-  display.display();
-  display.println(counter);
-  display.display();
+  display.setCursor(32, 16);
+  if (counter != current) {
+    display.clearDisplay();
+    display.display();
+    display.println(counter);
+    display.display();
+    current = counter;
+  }
 }
